c13af7af46c0adc0319772faff701003a41faee7
nguyenhuynhminhthuan265 <mty2605@gmail.com>
2020-01-02 12:32:30 +0700

    remove security1

--- src/main/java/com/myclass/controller/BillController.java
+++ src/main/java/com/myclass/controller/BillController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,10 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.myclass.dto.CustomUserDetails;
 import com.myclass.entity.Bill;
 import com.myclass.entity.DetailBill;
-import com.myclass.entity.Employee;
 import com.myclass.repository.BillRepository;
 import com.myclass.repository.DetailBillRepository;
 import com.myclass.repository.EmployeeRepository;
